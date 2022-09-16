fc8644e0129edcfc9de5a677251522a00c9e40c1
jigyanshu712 <104996908+jigyanshu712@users.noreply.github.com>
2022-09-16 18:57:38 +0530

    Add files via upload
    write a program to remove the first node of the list and insert it at the end ,without changing info part of any node.

---
+++ ques13.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+int info;
+struct node *next;
+};
+int main(){
+    int i,n,data;
+    struct node *temp,*p,*start,*q;
+    start=NULL;
+    printf("enter the number of nodes to be inserted : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        printf("enter the %d element : ",i);
+        scanf("%d",&data);
+        temp=(struct node *)malloc(sizeof(struct node));
+        temp->info=data;
+        if(start==NULL){
+            temp->next=start;
+            start=temp;
+        }
+        else{
+            p=start;
+            while(p->next!=NULL)
+                p=p->next;
+            temp->next=p->next;
+            p->next=temp;
+        }
+
+    }
+
+   q=start;
+   start=start->next;
+   q->next=NULL;
+   temp->next=q;
+   printf("the list is : \n");
+   p=start;
+   while(p!=NULL){
+    printf("%d\n",p->info);
+    p=p->next;
+   }
+return 0;
+}
